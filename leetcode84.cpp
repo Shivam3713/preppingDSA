00aed19f385dc136fe49d7a3a9b3d7ead1e0f1a3
Shivam S Prajapati <63530153+Shivam3713@users.noreply.github.com>
2026-07-31 15:29:44 +0530

    dp on squares

--- leetcode84.cpp
+++ leetcode84.cpp
@@ -4,30 +4,29 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int>left(n), right(n);
         stack<int>st;
+        vector<int>left(n), right(n);
         for(int i=n-1;i>=0;i--){
-            while( !st.empty() && heights[st.top()]>=heights[i]){
-                    st.pop();
-                }
-                right[i] = st.empty()?n:st.top();
+            while(!st.empty() && heights[st.top()]>=heights[i]) {
+                st.pop();
+            }
+            right[i]= st.empty()?n:st.top();
             st.push(i);
         }
         while(!st.empty()) st.pop();
-        // stack<int>st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()] >= heights[i] ){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            left[i] = st.empty()?-1:st.top();
+            left[i]=st.empty()?-1:st.top();
             st.push(i);
         }
-        int maxArea = INT_MIN;
+        int ans = INT_MIN;
         for(int i=0;i<n;i++){
-            int ans = (right[i]-left[i]-1)*heights[i];
-            maxArea = max(maxArea, ans);
+            int res = heights[i]*(right[i]-left[i]-1);
+            ans = max(ans, res);
         }
-        return maxArea;
+        return ans;
     }
 };
 int main(){
---
+++ leetcode85.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        
+    }
+};
+int main(){
+    vector<vector<int>>matrix=[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]];
+    Solution* s = new Solution();
+    cout<<s->maximalRectangle(matrix);
+
+}
